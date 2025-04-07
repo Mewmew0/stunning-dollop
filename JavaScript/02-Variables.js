82bca5ebf2d8d5be697065847ae0b2e8aa9ac0fa
Merin Akyüz <merinakyuz@gmail.com>
2025-04-07 12:09:49 +0300

    docs: add javascript add css-hw3

---
+++ JavaScript/02-Variables.js
@@ -0,0 +1,17 @@
+var lorem1 ="lorem"
+console.log(lorem1);
+lorem1 = "ipsum"
+
+let lorem2 = " ipsum"
+lorem1 = "lorem"
+document.write(lorem1+ lorem2)
+
+const $meow = " meow"
+
+let _lorem;
+
+Lorem = lorem1+lorem2+$meow
+
+console.log(Lorem)
+
+
